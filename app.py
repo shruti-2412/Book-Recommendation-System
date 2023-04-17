797e6c025236eeb22fcb422808f59b7cbd448f16
Shruti Chandak <99483160+shruti-2412@users.noreply.github.com>
2023-04-17 22:55:51 +0530

    files added

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import *
+import pickle
+
+popular_df= pickle.load(open('popular.pkl','rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def message():
+     return render_template("index.html",
+                            book_name= list(popular_df['Book-Title'].values),
+                            author=list(popular_df['Book-Author'].values),
+                            image=list(popular_df['Image-URL-M'].values),
+                            votes=list(popular_df['num_ratings'].values),
+                            rating=list(popular_df['avg_rating'].values)
+                            )
+
+if __name__ == '__main__':
+   app.run(debug = True)
